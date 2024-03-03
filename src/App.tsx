583a5bfe2f198d625e1585b38834899668076fb9
Ajeet Sinha <honeydreamssoftwares@gmail.com>
2024-03-04 01:13:21 +0530

    added dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from "@/components/ui/button"
 import {
@@ -9,11 +6,13 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion"
-
+import { ThemeProvider } from "@/components/theme-provider"
+import {ModeToggle} from "@/components/mode-toggle"
 
 export  function Home() {
   return (
     <div>
+      <ModeToggle></ModeToggle>
       <Button>Click me</Button>
       <Accordion type="single" collapsible>
   <AccordionItem value="item-1">
@@ -30,31 +29,13 @@ export  function Home() {
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <Home></Home>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+     <Home></Home>
+     </ThemeProvider>
+    
     </>
   )
 }
