d274ed2679437e17b5feb489d6115066f65b3cd2
Ajeet Sinha <honeydreamssoftwares@gmail.com>
2024-03-06 14:45:57 +0530

    components test

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,73 @@ import {
 import { ThemeProvider } from "@/components/theme-provider"
 import {ModeToggle} from "@/components/mode-toggle"
 import {ProfileForm} from "./ProfileForm"
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  //NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  //NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+
+import { TypewriterEffect } from "@/components/ui/typewriter-effect";
+
+
+
+export function TypewriterEffectDemo() {
+  const words = [
+    {
+      text: "Build",
+    },
+    {
+      text: "awesome",
+    },
+    {
+      text: "apps",
+    },
+    {
+      text: "with",
+    },
+    {
+      text: "Honeydreams.",
+      className: "text-blue-500 dark:text-blue-500",
+    },
+  ];
+  return (
+    <div className="flex flex-col items-center justify-center h-[40rem] ">
+      <p className="text-neutral-600 dark:text-neutral-200 text-base  mb-10">
+        The road to freedom starts from here
+      </p>
+      <TypewriterEffect words={words} />
+      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 mt-10">
+        <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
+          Join now
+        </button>
+        <button className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">
+          Signup
+        </button>
+      </div>
+    </div>
+  );
+}
+
+
 export  function Home() {
   return (
     <div>
+      <NavigationMenu>
+  <NavigationMenuList>
+    <NavigationMenuItem>
+      <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+      <NavigationMenuContent>
+        <NavigationMenuLink>Link</NavigationMenuLink>
+      </NavigationMenuContent>
+    </NavigationMenuItem>
+  </NavigationMenuList>
+</NavigationMenu>
+
       <ModeToggle></ModeToggle>
       <Button>Click me</Button>
       <Accordion type="single" collapsible>
@@ -23,6 +87,8 @@ export  function Home() {
   </AccordionItem>
 </Accordion>
 <ProfileForm></ProfileForm>
+
+<TypewriterEffectDemo></TypewriterEffectDemo>
     </div>
   )
 }
